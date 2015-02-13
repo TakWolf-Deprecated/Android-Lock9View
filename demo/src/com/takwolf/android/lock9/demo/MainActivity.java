410421767af1a2dbd28439c5a7250c500e10cd63
TakWolf <takwolf@foxmail.com>
2015-02-14 00:53:59 +0800

    target=android-21

--- demo/src/com/takwolf/android/lock9/demo/MainActivity.java
+++ demo/src/com/takwolf/android/lock9/demo/MainActivity.java
@@ -18,7 +18,7 @@ public class MainActivity extends Activity {
         setContentView(R.layout.activity_main);
         lock9View = (Lock9View) findViewById(R.id.lock_9_view);
         lock9View.setCallBack(new CallBack() {
-            
+
             @Override
             public void onFinish(String password) {
                 Toast.makeText(MainActivity.this, password, Toast.LENGTH_SHORT).show();
